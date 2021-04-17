46fd5afdcd0965f738938bce2677afcc218d0ca7
Matuesz <bieganski.m@wp.pl>
2021-04-18 01:28:43 +0200

    added basic winning stuff.

--- ex1/convert_csv.sh
+++ ex1/convert_csv.sh
@@ -6,9 +6,14 @@ set -x
 
 
 data_fname=clash_royale_games_Jan_2019.csv
-num=100000
-res_fname="res.csv"
 
+num=100000
+res_fname="winning.csv"
+
+if [ -f $res_fname ]; then
+	echo "error: $res_fname already exists!"
+	exit 1
+fi
 
 
 header=`head -n 1 $data_fname`
@@ -16,12 +21,16 @@ header=`head -n 1 $data_fname`
 
 
 BASIC=0
+WINNING=0
 
 while [ "$1" != "" ]; do
     case $1 in
         --basic)
             BASIC=1
             ;;
+	--winning)
+	    WINNING=1
+	    ;;
         *)
 	    echo "ARGS ERROR"
 	    exit 1
@@ -41,11 +50,19 @@ tmp=`mktemp`
 
 # skip header
 tail -n +2 $data_fname > $tmp
-shuf -n $num $tmp >> $res_fname
+shuf $tmp >> $res_fname
+
+# in 'res_fname' there is pure, shufled data.
+
+if [ $WINNING -eq 1 ]; then
+	echo $header > $tmp
+	grep ",1," $res_fname | head -n $num >> $tmp
+	cat $tmp > $res_fname
+fi
 
 if [ $BASIC -eq 1 ]; then
         cmd="cut -d , -f 1,5"
-	cat $res_fname | $cmd | tr '_' ',' > $tmp
+	cat $res_fname | head -n $num | $cmd | tr '_' ',' > $tmp
 	cat $tmp > $res_fname
 fi
 echo "Done! file $res_fname is ready."
